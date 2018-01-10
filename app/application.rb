979aa51ed2fa48cf64d36af83a5df517721a2a4a
Yomaira Escano <yjescano@gmail.com>
2018-01-10 02:38:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     # num_1 = Kernel.rand(1..20)
     # num_2 = Kernel.rand(1..20)
     # num_3 = Kernel.rand(1..20)
-    # 
+    #
     # resp.write "#{num_1}\n"
     # resp.write "#{num_2}\n"
     # resp.write "#{num_3}\n"
