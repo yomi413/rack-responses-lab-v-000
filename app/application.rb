27ace10230773737d60d63a2f0e7ef31bc71af11
Yomaira Escano <yjescano@gmail.com>
2018-01-10 02:39:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
     # resp.write "#{num_1}\n"
     # resp.write "#{num_2}\n"
     # resp.write "#{num_3}\n"
+    binding.pry
 
     if num_1 == num_2 && num_2 == num_3
       resp.write "You Win"
