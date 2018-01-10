01352daa59c3f1e3d7b5512b07971e18939bad6d
Yomaira Escano <yjescano@gmail.com>
2018-01-10 02:43:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
     # resp.write "#{num_2}\n"
     # resp.write "#{num_3}\n"
 
-    if num_1 == num_2 && num_2 == num_3
+    if time 
       resp.write "You Win"
     else
       resp.write "You Lose"
