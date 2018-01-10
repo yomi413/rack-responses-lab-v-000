1961657a31d65e23eaac73f9fe4220ac9a3ab318
Yomaira Escano <yjescano@gmail.com>
2018-01-10 03:24:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
     # resp.write "#{num_3}\n"
     # binding.pry
 
-    if Time.new < 12
+    if Integer(Time.new) < 12
       resp.write "Good morning!"
     else
       resp.write "Good afternoon!"
