a7c312a36137dc705363286b597987e7ded05ff7
Yomaira Escano <yjescano@gmail.com>
2018-01-10 05:40:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,18 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    # num_1 = Kernel.rand(1..20)
-    # num_2 = Kernel.rand(1..20)
-    # num_3 = Kernel.rand(1..20)
-    #
-    # resp.write "#{num_1}\n"
-    # resp.write "#{num_2}\n"
-    # resp.write "#{num_3}\n"
-    # binding.pry
-
     time = Time.now
-    # time.hour(0..23)
-
 
     if time.hour < 12
       resp.write "Morning"
