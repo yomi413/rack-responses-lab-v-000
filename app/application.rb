0144375a84d1fab40de0bf86067b0d592998a7be
Yomaira Escano <yjescano@gmail.com>
2018-01-10 03:25:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
     # resp.write "#{num_3}\n"
     # binding.pry
 
-    if Integer(Time.new) < 12
+    if Integer(Time.new) <= 12
       resp.write "Morning"
     else
       resp.write "Afternoon"
